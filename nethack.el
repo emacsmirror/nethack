d502aef3ca6ad86beb05d51279ec0cf907bbb0ff
George Huebner <george@feyor.sh>
2025-09-13 12:44:50 -0500

    bump version

--- nethack.el
+++ nethack.el
@@ -799,7 +799,7 @@ buffer."
 
 
 ;;; VERSION:
-(defconst nethack-el-version "0.13.3")
+(defconst nethack-el-version "0.14.0")
 (defconst nethack-el-earliest-compatible-version "0.12.0")
 (defun nethack-el-version ()
   (interactive)
